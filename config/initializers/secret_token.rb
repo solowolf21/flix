636e764306a210ec07b24209eb91ec4beddec521
Sen Zhang <sen.zhang@appfolio.com>
2013-11-15 17:57:29 -0800

    sz - first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flix::Application.config.secret_token = '5a7dc5fd20e9a13c398c82bbe8a721360dc1301c6ca0256ce9738f5ced092ea02c64ea1cbf8701b06e03c22651e455799dd7ec47bee0bdad97868c74132dbf8a'
